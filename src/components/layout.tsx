4acb3ccfb87e040e0fb2bae4830a243a76f06900
TrixiS <oficialmorozov@gmail.com>
2023-06-05 04:53:55 +0300

    Add Header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,31 @@
+import Link, { type LinkProps } from "next/link";
 import { PropsWithChildren } from "react";
 
 const Header: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <header className="flex flex-col border-b border-background-accent p-4">
       <div className="flex flex-row gap-x-4 tracking-tight text-background-accent">
-        <div className="mr-4 font-bold text-foreground-accent">
+        <Link href="/" className="mr-4 font-bold text-foreground-accent">
           TrixiS' Blog
-        </div>
+        </Link>
         {children}
       </div>
     </header>
+  );
+};
+
+const HeaderLink: React.FC<LinkProps & PropsWithChildren> = ({
+  children,
+  ...rest
+}) => {
+  return (
+    <Link
+      className="transition-colors hover:text-foreground"
+      target="_blank"
+      {...rest}
+    >
+      {children}
+    </Link>
   );
 };
 
@@ -17,11 +33,11 @@ export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <main className="flex min-h-screen flex-col overflow-y-auto bg-background text-foreground">
       <Header>
-        <div>Github</div>
-        <div>Telegram</div>
-        <div>Kwork</div>
+        <HeaderLink href="https://github.com/TrixiS">Github</HeaderLink>
+        <HeaderLink href="https://t.me/trixis_std">Telegram</HeaderLink>
+        <HeaderLink href="https://kwork.ru/user/trixis_">Kwork</HeaderLink>
       </Header>
-      <div className="mx-auto mt-4 flex w-11/12 flex-col sm:w-10/12">
+      <div className="mx-auto my-8 flex w-11/12 flex-col sm:w-10/12 md:my-16">
         {children}
       </div>
     </main>
